74b2bc0ba4633fada32f6235ce7f7a1d1de2b478
Vlad Serbu <vlad.serbu@touchsurgery.com>
2019-11-13 17:23:23 +0000

    fixed the current value

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -29,6 +29,6 @@ class ViewController: UIViewController {
         present(alert, animated: true, completion: nil)
 }
     @IBAction func sliderMoved(_ slider: UISlider) {
-        print("The value of the slider is now: \(slider.value)")
+        currentValue = lroundf(slider.value)
     }
 }
