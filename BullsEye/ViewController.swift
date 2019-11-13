12698d68c9b216c682c4eddfb457ba5cc2f4dd9a
Vlad Serbu <vlad.serbu@touchsurgery.com>
2019-11-13 16:51:24 +0000

    Moved the message variable inside the alert popup

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,17 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var currentValue: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     @IBAction func showAlert() {
+        let message = "The value of the slider is: \(currentValue)"
         let alert = UIAlertController(title: "Hello world",
-                                      message: "This is my first app",
+                                      message: message,
 
                                       preferredStyle: .alert)
-        let action = UIAlertAction(title: "Amazing",
+        let action = UIAlertAction(title: "Ok",
                                    style: .default,
                                    handler: nil)
         alert.addAction(action)
